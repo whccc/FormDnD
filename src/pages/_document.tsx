a84810be9b4afb178f06060a666c1e9ecd9c3cf1
Wilson Castro <wilsoncastro0710@gmail.com>
2021-03-30 16:02:39 -0500

    Config Styled Components

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,24 +1,28 @@
-import Document, { DocumentContext, Html, Head, Main, NextScript } from 'next/document';
 import React from 'react';
-
-class MyDocument extends Document {
+import Document, { DocumentContext } from 'next/document';
+import { ServerStyleSheet } from 'styled-components';
+export default class MyDocument extends Document {
   static async getInitialProps(ctx: DocumentContext) {
-    const initialProps = await Document.getInitialProps(ctx);
+    const sheet = new ServerStyleSheet();
+    const originalRenderPage = ctx.renderPage;
+    try {
+      ctx.renderPage = () =>
+        originalRenderPage({
+          enhanceApp: (App) => (props) => sheet.collectStyles(<App {...props} />),
+        });
+      const initialProps = await Document.getInitialProps(ctx);
 
-    return initialProps;
-  }
-
-  render() {
-    return (
-      <Html>
-        <Head />
-        <body>
-          <Main />
-          <NextScript />
-        </body>
-      </Html>
-    );
+      return {
+        ...initialProps,
+        styles: (
+          <>
+            {initialProps.styles}
+            {sheet.getStyleElement()}
+          </>
+        ),
+      };
+    } finally {
+      sheet.seal();
+    }
   }
 }
-
-export default MyDocument;
